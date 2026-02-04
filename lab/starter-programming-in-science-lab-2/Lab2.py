8d00999147bd77ddc51ef17f2cb9f3c69c6966f1
Chelsea Gila Vanounou <chelseagilavanounou@icloud.com>
2026-02-03 21:24:02 -0500

    Completed Lab 2: height and car distance functions

--- lab/starter-programming-in-science-lab-2/Lab2.py
+++ lab/starter-programming-in-science-lab-2/Lab2.py
@@ -2,10 +2,10 @@
 # This function should take the initial height h0 and time t as inputs, and return the height at time t.
 def calculate_height(h0, t):
     # TODO: Implement this function
-    pass  # Replace with your code
+    return round(h0 - 0.5 * 9.8 * (t ** 2), 1)
 
 # Function 2: Calculate the distance traveled by the car
 # This function should take the time t as input and return the distance traveled by the car.
 def calculate_car_distance(t):
     # TODO: Implement this function
-    pass  # Replace with your code
+    return 20 * t
